1fd64034dae70a539c306bc3280af468f91db131
spaz <saisrikark@gmail.com>
2019-11-17 19:45:28 +0530

    Added namespaces

--- app.js
+++ app.js
@@ -6,11 +6,32 @@ server = app.listen(port);
 
 const io = require('socket.io')(server);
 
+hashCode = function(s) {
+	var h = 0, l = s.length, i = 0;
+	if ( l > 0 )
+	  while (i < l)
+		h = (h << 5) - h + s.charCodeAt(i++) | 0;
+	return h;
+  };
+
+function generateCollabId() {
+	var collabId = "";
+	var date = new Date();
+	collabId = date.toTimeString()
+	collabId = hashCode(collabId)
+	return collabId
+}
+
+
 io.on('connection', (socket) => {
 	console.log('New user connected') 
 
 	socket.on('sendDelta', (delta) => {
 		socket.broadcast.emit('applyDelta',delta);
+	})
+
+	socket.on('collabId', function(){
+		socket.emit(generateCollabId())
 	})
 });
 
