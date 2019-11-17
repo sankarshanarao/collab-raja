3b6bc6e5db942cac06c185728ea23b4983445e56
Sankarshana H Rao <sankarshana.rao@gmail.com>
2019-11-17 22:31:43 +0530

    Added all the styling

--- collab-raja-ui/src/App.js
+++ collab-raja-ui/src/App.js
@@ -3,17 +3,21 @@ import ReactQuill from 'react-quill';
 import './App.css';
 import 'react-quill/dist/quill.snow.css';
 import kingImg from './773-27-512.png';
+import CollabWidget from './collab-widget';
 
 function App() {
   return (
     <div className="App">
       <div className="navbar">
+        <div className='widget-cont'>
+          <CollabWidget />
+        </div>
         <img src={kingImg} alt="king" className="king-icon" />
+
       </div>
-      <div className='quill-container'> 
-        <ReactQuill className="quillEditor" theme="snow"/>
-      </div>  
-      
+      <div className='quill-container'>
+        <ReactQuill className="quillEditor" theme="snow" />
+      </div>
     </div>
   );
 }
